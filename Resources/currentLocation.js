2af6bce94d6e050938149d7e6c7a76c581f4895a
hexxorleiva <hexxorleiva@gmail.com>
2012-07-30 16:38:31 -0300

    Update Resources/currentLocation.js
    Adding alerts to where there might be an error in getting a GPS signal so that the user may know that there is something wrong.

--- Resources/currentLocation.js
+++ Resources/currentLocation.js
@@ -12,15 +12,18 @@ if (Titanium.Geolocation.locationServicesEnabled)
 	Titanium.API.info('Geolocation services are on.');
 	} else {
 	Titanium.UI.createAlertDialog({title:'Geolocation Off', message:'Your device has location services turned off - please turn it on.'}).show();
-}; //end of Alert to see if you have geolocaiton turned on.
+}; //	end of Alert to see if you have geolocaiton turned on.
 
 function currentLocation(_callback){
 	Ti.Geolocation.getCurrentPosition(function(e) {
 		if (e.error) {
 			Ti.API.error('Can not get your current location: ' + e.error);
-			//if (_callback) {
-			//	_callback(null);
-			//}
+//  If the App can not get a current position when it has launched, we need to tell the user there is a problem.
+			var lostSignal = Ti.UI.createAlertDialog({
+			title:'GPS unavailable',
+			message:'Check to see that you have a phone signal or Wi-Fi connection.'
+			});
+			lostSignal.show();
 		} else {
 			//Ti.API.info('got a location from currentPosition from function currentLocation :' + JSON.stringify(e));
 			//Ti.App.fireEvent('current.position',e.coords);
@@ -39,10 +42,14 @@ function movingLocation(_callback){
 	Ti.Geolocation.addEventListener('location', function(e) {
 		if (e.error) {
 		Ti.API.error('Error: ' + e.error);
+	//  If the App can not get a current position when it has launched, we need to tell the user there is a problem.
+		var lostSignal = Ti.UI.createAlertDialog({
+		title:'GPS unavailable',
+		message:'Check to see that you have a phone signal or Wi-Fi connection.'
+		});
+		lostSignal.show();
 		return;
 		} else {
-		//Ti.API.info('App.fireEvent of location.updated successful : ' + JSON.stringify(e.coords));
-		//Ti.App.fireEvent('location.updated',e.coords);
 		
 		if (_callback) {
 			_callback(e.coords);
